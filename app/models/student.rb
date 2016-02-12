8b9f1401aa046b1554f105f063b5080a4bb0b752
ravimariya <rm9492@outlook.com>
2016-02-12 11:17:33 +0530

    modified role_models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
 	belongs_to :standard
-validates:enrollment_no,presence:true,uniqueness:true
+	validates:enrollment_no,presence:true,uniqueness:true
 	validates:first_name,presence:true
 	validates:last_name,presence:true
 	validates:date_of_birth,presence:true
@@ -8,7 +8,7 @@ validates:enrollment_no,presence:true,uniqueness:true
 	validates:city,presence:true
 	validates:contactno,presence:true,numericality:{only_integer:true}
 	validates:standard_id,presence:true
-	validates:enrollment_no,presence:true,length:{is:12}
+	validates:enrollment_no,presence:true,length:{is:4}
 	validates:password,confirmation:true,presence:true
 	validates:password_confirmation,presence:true
 end
