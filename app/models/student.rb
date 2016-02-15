c641eec1303be53680804ea646c2262ce42cbfa2
Shailes <sp98484gmail.com>
2016-02-15 16:10:21 +0530

    Student validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,15 @@
 class Student < ActiveRecord::Base
 	belongs_to:standard
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+	validates:enrollment_no,presence:true,uniqueness:true,length:{is:4}
+	validates:first_name,presence:true
+	validates:last_name,presence:true
+	validates:date_of_birth,presence:true
+	validates:address,presence:true
+	validates:city,presence:true
+	validates:contactno,presence:true,numericality:{only_integer:true}
+	validates:standard_id,presence:true
+	
   devise :database_authenticatable, :registerable,:trackable, :validatable, :authentication_keys=>[:enrollment_no]
-
   def email_required?
   false
 	end
