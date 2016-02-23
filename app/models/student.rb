3b86562932b82c8401f0753ec5186bf4a39df970
ravimariya <rm9492@outlook.com>
2016-02-23 11:17:40 +0530

    fixed indentation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,27 +1,17 @@
 class Student < ActiveRecord::Base
 	belongs_to:standard
-	validates:first_name,presence:true
-	validates:middle_name,presence:true
-	validates:last_name,presence:true
-	validates:date_of_birth,presence:true
-	validates:address,presence:true
-	validates:city,presence:true
-	validates:contactno,presence:true,numericality:{only_integer:true}
-	validates:standard_id,presence:true
-
-
-	devise :database_authenticatable, :trackable, :validatable, :authentication_keys=>[:enrollment_no]
-	def email_required?
-		false
-	end
-
-
+	validates :first_name, :middle_name, :last_name, :date_of_birth, :address, :city, :contactno, :standard_id, presence:true
 	has_attached_file :avatar, :styles => {:original => "150x150"},
 	:path => ":rails_root/public/avatars/students/:styles/:basename.:extension"
-
 	validates_attachment_presence :avatar
 	validates_attachment_size :avatar, :less_than => 300.kilobytes
 	validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
+
+	devise :database_authenticatable, :trackable, :validatable, :authentication_keys=>[:enrollment_no]
+
+	def email_required?
+		false
+	end
 
 	before_post_process :rename_avatar
 
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,16 +1,9 @@
 class Teacher < ActiveRecord::Base
 	devise :database_authenticatable,:trackable, :validatable
-	validates:first_name,presence:true
-	validates:middle_name,presence:true
-	validates:last_name,presence:true
-	validates:dateofbirth,presence:true
-	validates:address,presence:true
-	validates:city,presence:true
-	validates:contactno,presence:true,numericality:{only_integer:true}
-
+	validates :first_name, :middle_name, :last_name, :dateofbirth, :address, :city, presence:true
+	validates :contactno, presence:true, numericality:{only_integer:true}
 	has_attached_file :avatar, :styles => {:original => "150x150"},
 	:path => ":rails_root/public/avatars/teachers/:styles/:basename.:extension"
-
 	validates_attachment_presence :avatar
 	validates_attachment_size :avatar, :less_than => 300.kilobytes
 	validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
