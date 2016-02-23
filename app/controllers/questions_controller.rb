a1dbc868b7b1706c03abba8a31a05ab6299dbb54
ami faldu <amiprincess7@gmai.com>
2016-02-23 17:36:31 +0530

    change in dabase.yml

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,35 +1,35 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_teacher!
   def index
-   @questions = Question.all
-   end
-   def new
-   @question = Question.new
-   end
-   def create
-   @question = Question.new(params_arg)
-    if @question.save
-       render:'show'
-     else 
-         render:'new'
-       end
+     @questions = Question.all
+  end
+  def new
+    question = Question.new
+  end
+ def create
+  @question = Question.new(params_arg)
+  if @question.save
+    render:'show'
+  else 
+    render:'new'
+  end
+ end
+ def edit
+  @question = Question.find(params[:id])
+ end
+ def update
+  @question = Question.find(params[:id])
+    if @question.update(params_arg)
+      render:'show'
+    else render:'edit'
     end
-    def edit
-	  @question = Question.find(params[:id])
-    end
-    def update
-	  @question = Question.find(params[:id])
-        if @question.update(params_arg)
-          render:'show'
-        else render:'edit'
-        end
-    end
-    def destroy
-	  @question = Question.find(params[:id])
-	  @question.destroy
-      redirect_to questions_path
-     end
-     def show
+ end
+ def destroy
+	@question = Question.find(params[:id])
+	@question.destroy
+  redirect_to questions_path
+ end
+ def show
 	  @question = Question.find(params[:id])
     end
     private
