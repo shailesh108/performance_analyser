95cfb94893234c8365e572d861d53a65019c02cb
ami faldu <amiprincess7@gmai.com>
2016-02-23 13:07:10 +0530

    modification in view

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < ActiveRecord::Base
-has_many :teacher_standard_subjects
-has_many :tests
-devise :database_authenticatable,:trackable, :validatable,:registerable
-validates :first_name,:middle_name,:last_name,:dateofbirth,:address,:city,presence:true
-validates :contactno,presence:true,numericality:{only_integer:true}
+  has_many :teacher_standard_subjects
+  has_many :tests
+  devise :database_authenticatable,:trackable, :validatable,:registerable
+  validates :first_name,:middle_name,:last_name,:dateofbirth,:address,:city,presence:true
+  validates :contactno,presence:true,numericality:{only_integer:true}
 end
