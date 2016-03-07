bafe812ab3bf946ca2ceae4d87c1dea996f71778
ravimariya <rm9492@outlook.com>
2016-03-07 10:37:02 +0530

    give test button activation and deactivation

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,16 +1,15 @@
 module StudentsHelper
   def test_status(test)
-
     time_now = Time.now.strftime("%d-%m-%Y %l:%M %p")
     test_start_time = test.test_datetime.strftime("%d-%m-%Y %l:%M %p")
     test_finish_time = (test.test_datetime + test.total_time.minutes).strftime("%d-%m-%Y %l:%M %p")
-   
+
     if (test_start_time > time_now)
-      return link_to("Pending".html_safe,starttest_students_path(test),:class => "btn btn-danger btn-xs disabled") 
-    elsif (test_start_time <= time_now && test_finish_time >= time_now) 
-       return link_to("Pending".html_safe,starttest_students_path(test),:class => "btn btn-danger btn-xs")
-     else
-       return link_to("Complete",{},:class => "btn btn-success btn-xs")         
+      return "<a class='btn btn-danger btn-xs disabled'>Pending</a>".html_safe 
+    elsif (test_start_time <= time_now && time_now < test_finish_time) 
+      return link_to("Pending".html_safe,starttest_students_path(test),:class => "btn btn-danger btn-xs")
+    else
+      return "<a class='btn btn-success btn-xs'>Complete</a>".html_safe 
     end
   end
 
@@ -24,6 +23,4 @@ module StudentsHelper
   def get_student_avatar
     image_tag("/avatars/students/originals/" + @student.avatar_file_name)
   end
-
 end
-
